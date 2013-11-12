1a64aea1f90b44600a8db79f475db5faae018cb4
Turupawn <antares_43@hotmail.com>
2013-11-12 16:44:09 -0600

    First commit

---
+++ STG.h
@@ -0,0 +1,38 @@
+#ifndef STG_H
+#define STG_H
+
+#include "TinyXml/tinyxml.h"
+#include "RosalilaGraphics/RosalilaGraphics.h"
+#include "RosalilaSound/RosalilaSound.h"
+#include "RosalilaInputs/RosalilaInputs.h"
+#include "Character/Character.h"
+#include "Character/Player.h"
+#include "Character/Enemy.h"
+#include "Stage/Stage.h"
+#include "RosalilaGraphics/Animation.h"
+
+class STG
+{
+    Sound* sonido;
+    RosalilaGraphics* painter;
+    Receiver* receiver;
+    Player*player;
+    Enemy*enemy;
+    Stage*stage;
+
+    string username;
+
+    Animation you_win,you_loose;
+    int iteration;
+
+    bool isOutOfBounds(int pos_x,int pos_y);
+    void mainLoop();
+    void logic();
+    void render();
+    void deletePatterns(int stage_bound_x1,int stage_bound_y1,int stage_bound_x2,int stage_bound_y2);
+    void checkCharacterOutOfBounds();
+public:
+    STG(Sound* sonido,RosalilaGraphics* painter,Receiver* receiver,Player*player,Enemy*enemy,Stage*stage);
+};
+
+#endif
